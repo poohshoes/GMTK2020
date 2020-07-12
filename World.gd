bc1a8044f620a9d71c1e9cf4aad82669fa5dd396
Willem Klumpenhouwer <willemjjk@gmail.com>
2020-07-12 13:08:08 -0400

    Added balancing, potatoes!

--- World.gd
+++ World.gd
@@ -32,11 +32,12 @@ func tick():
 	
 	
 	# Start with a bunch of regional stuff (see region code)
-	var potatoesPerDriver = 5
+	var potatoesPerDriver = 20
 	var exportsByRegion = []
 	var importsByRegion = []
 	
-	var inevitability = alpha * pow(action, beta)
+	var inevitability = alpha * pow(action, beta) + 1
+	print("Inevitability: " + str(inevitability))
 # warning-ignore:unused_variable
 	for i in range(5):
 		exportsByRegion.append(0)
